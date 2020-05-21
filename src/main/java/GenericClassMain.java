80386ccceaa61aba49c5a4b6a503da538623dc00
Manqasana-Lepota <nqhasy@gmail.com>
2020-05-21 14:57:26 +0200

    second commit

--- src/main/java/GenericClassMain.java
+++ src/main/java/GenericClassMain.java
@@ -1,23 +1,50 @@
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-public class GenericClassMain {
-    //creating object of a class Logger
-    private static Logger logger = LogManager.getLogger(GenericClassMain.class.getName());
+public class GenericClassMain<T> {
+
+    //A variable of type T is declared
+    private T studInfo;
+
+    //set method
+    public void set(T studInfo) {
+        this.studInfo = studInfo;
+    }
+
+    //get method
+    public T get() {
+        return studInfo;
+    }
+
+    public static <T> void printArrayContent(T[] studInfo ) {
+        //Display array elements
+        for(T type : studInfo) {
+            System.out.printf("%s ", type);
+        }
+        System.out.println();
+    }
 
 
     public static void main(String[] args) {
 
-        //displaying
-        logger.info("********Student Information***********");
+        //Create arrays of Integer and String
+        Integer[] intArrayStudNumbers = { 2001, 2002, 2003, 2004, 2005 };
+        String[] stringArrayStuNames = { "Lwandle", "Lerato", "Nqobile", "Kwanele" };
 
-        GenericClass<String> studName = new GenericClass<>();//instance of String type
+        System.out.println("\nArray intArrayStudNumbers contains:");
+        printArrayContent(intArrayStudNumbers);// pass an Integer array
+
+
+        System.out.println("\nArray stringArrayStuNames contains:");
+        printArrayContent(stringArrayStuNames);// pass an String array
+
+
+
+        GenericClassMain<String> studName = new GenericClassMain<>();//instance of String type
         studName.set("Manqasana Lepota");//set student name
-        logger.debug("Student Name : " + studName.get());//print student name
+        System.out.println("\nStudent Name : " + studName.get());//print student name
 
 
-        GenericClass<Integer> studNumber = new GenericClass<>();//instance of type Integer
-        studNumber.set(20200410);//set student number
-        logger.debug("Student Number : " + studNumber.get());//print student number
+        GenericClassMain<Integer> studNumber = new GenericClassMain<>();//instance of type Integer
+        studNumber.set(2006);//set student number
+        System.out.println("Student Number : " + studNumber.get());//print student number
     }
 }
